6d8a341c1569c43dab59ffe4e67cd10d6ce7aa0d
Sooki <0xsooki@protonmail.com>
2024-11-20 15:25:24 +0200

    update function binding in pybind

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "numpy_utils.hpp"
 #include "permanent.hpp"
 #include <complex>
-#include <math.h>
 #include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
@@ -12,13 +11,14 @@ namespace py = pybind11;
 
 template <typename T>
 T permanent_np(py::array_t<T, py::array::c_style | py::array::forcecast> array,
-               py::array_t<int> row_mult_arr, py::array_t<int> col_mult_arr) {
+               py::array_t<int> cols, py::array_t<int> rows) {
   Matrix<T> matrix = numpy_to_matrix(array);
 
-  std::vector<int> row_mult = numpy_to_vec(row_mult_arr);
-  std::vector<int> col_mult = numpy_to_vec(col_mult_arr);
+  std::vector<int> row_mult = numpy_to_vec(rows);
+  std::vector<int> col_mult = numpy_to_vec(cols);
 
-  T result = permanent<T>(matrix, row_mult, col_mult);
+  T result =
+      permanent<std::complex<double>, double>(matrix, col_mult, row_mult);
 
   return result;
 }
@@ -35,7 +35,6 @@ PYBIND11_MODULE(_core, m) {
 
            permanent
     )pbdoc";
-
   m.def("permanent", &permanent_np<std::complex<double>>, R"pbdoc(
         Compute the permanent of a matrix with specified row and column multiplicities.
     )pbdoc");
