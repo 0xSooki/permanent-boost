b842176f0d8a7c802d96d6c5753e6d7e250452a8
Sooki <0xsooki@protonmail.com>
2025-02-13 20:55:50 +0100

    add xla handler to imports

--- src/sooki/__init__.py
+++ src/sooki/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, permanent
+from ._core import __doc__, __version__, permanent, registrations
 
 __all__ = ["__doc__", "__version__",
-           "permanent"]
+           "permanent", "registrations"]
